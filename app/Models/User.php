f55cb00f43858ef658115843854bd3446cb80e26
Bitolko <hristijan.hbo@gmail.com>
2026-05-30 01:06:33 +1000

    QA fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'preferred_language', 'is_admin'])]
+#[Fillable(['name', 'email', 'password', 'preferred_language'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
